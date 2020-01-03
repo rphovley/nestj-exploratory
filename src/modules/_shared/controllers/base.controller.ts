128ea06033498474f9028adcc50ce490c27bb9d9
cazlu8 <cazlubios8@gmail.com>
2020-01-03 21:43:02 -0200

    create the dynamic loader modules, setup the security middlewares.

--- src/modules/_shared/controllers/base.controller.ts
+++ src/modules/_shared/controllers/base.controller.ts
@@ -1,8 +1,8 @@
-import { UseFilters, ClassSerializerInterceptor, UseInterceptors } from "@nestjs/common";
-import { HttpExceptionFilter } from "src/filters/http.exception-filter";
+import { UseFilters, ClassSerializerInterceptor, UseInterceptors } from '@nestjs/common';
+import { HttpExceptionFilter } from 'src/filters/http.exception-filter';
 
 @UseFilters(HttpExceptionFilter)
 @UseInterceptors(ClassSerializerInterceptor)
 export abstract class BaseController{
-    
+
 }
